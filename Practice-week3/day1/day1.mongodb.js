ef2aeeb75631439ce4cdf110b95fbe950288ae3f
prabhatX7 <legendprabhat940@gmail.com>
2026-09-16 18:50:50 +0545

    week add vayo

---
+++ Practice-week3/day1/day1.mongodb.js
@@ -0,0 +1,43 @@
+// use("Todo")
+
+// db.Day1.insertOne({
+//     name: "Prabhat",
+//     age: 20,
+//     contact : 9758452536
+
+// })
+
+// db.Day1.insertOne({
+//     "name:"yabesh",
+//     age: 21,
+//     contact: 6585964512
+// })
+
+// db.Day1.insertMany([
+//     {name: "Prabhat", "age":21},
+//     {name: "Bipin", "age":23}
+// ])
+
+// db.Day1.findAndModify({
+//     name: "Bipin",
+//     age: 20
+// })
+
+// db.Day1.findOne ({
+//     name: "Prabhat",
+//     age: 21
+// })
+
+// db.Day1.updateMany(
+//     { name: "Bipin" },
+//     {
+//         $set: {
+//             age: 21,
+//             contact : 54654146543,
+//         }
+//     }
+// )
+
+// db.Day1.deleteOne({
+//     name: "yabesh"
+// })
